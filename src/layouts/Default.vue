df748131fb89a31a73d3cf3fe2e212820e604076
Ethan Barrett <hello@buildingsareheavy.com>
2019-04-18 23:51:17 -0600

    Put the navigation back, I'll try to deploy on Netlify again in the morning. The yarn.lock file hasn't changed in a while.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,6 +7,7 @@
       </div>
 
       <div class="header__right">
+        <g-link class="navigation" to="/">Videos</g-link>
         <g-link class="navigation" :to="{ name: 'info' }">Info</g-link>
         <g-link class="navigation" :to="{ name: 'interviews' }">Interviews</g-link>
       </div>
